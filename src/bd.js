61530049687746ec8ea58acdabfa14dd7b48359b
Bakdoolot Altybaev <altybaevbaku15@gmail.com>
2022-01-03 13:42:45 +0600

    Create local data base

---
+++ src/bd.js
@@ -0,0 +1,19 @@
+export const navLists = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Library",
+    url: "library",
+  },
+];
+
+export const icons = [
+  {
+    home: "./assets/icons/home-icon.svg",
+    lib: "./assets/icons/list-icon.svg",
+  },
+];

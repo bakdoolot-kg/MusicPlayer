82e7df168128ef266798f6850eb2cf45ae2da301
Bakdoolot Altybaev <altybaevbaku15@gmail.com>
2022-01-03 13:44:41 +0600

    add App file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./app.scss";
+import { Header } from "./components";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;

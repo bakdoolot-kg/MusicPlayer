d611825ccf7d772cef96c63f82a9afe398554e30
bakdoolot-kg <altybaevbaku15@gmail.com>
2022-01-08 03:12:37 +0600

    add submenu titles & url

--- src/bd.js
+++ src/bd.js
@@ -11,6 +11,29 @@ export const navLists = [
   },
 ];
 
+export const submenu = [
+  {
+    id: 1,
+    title: "Songs",
+    url: "songs",
+  },
+  {
+    id: 2,
+    title: "Playlists",
+    url: "playlists",
+  },
+  {
+    id: 3,
+    title: "Albums",
+    url: "albums",
+  },
+  {
+    id: 4,
+    title: "Artists",
+    url: "artists",
+  },
+];
+
 export const icons = [
   {
     home: "./assets/icons/home-icon.svg",

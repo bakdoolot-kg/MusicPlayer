dc923f83b47fa94bd7f6e9119d1fc196c0a3c21f
bakdoolot-kg <altybaevbaku15@gmail.com>
2022-01-15 00:56:16 +0600

    Update MiniPlayer

--- src/components/MiniPlayer/MiniPlayer.jsx
+++ src/components/MiniPlayer/MiniPlayer.jsx
@@ -1,39 +1,30 @@
 import React, { useState } from "react";
 import "./miniPlayer.scss";
 import { Wave, PlayList, CoverMusic } from "../";
-// import Wave from "../Wave/Wave";
-// import PlayList from "../Playlist/PlayList";
 import { tracks } from "../../bd";
-// import CoverMusic from "../CoverMusic/CoverMusic";
-
-// const url = "https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3";
-
-// const tracks = [
-//     {
-//         id: 0,
-//         title: "Brahms: St Anthony Chorale - Theme, Two Pianos Op.56b",
-//         url:
-//             "https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3"
-//     },
-//     {
-//         id: 1,
-//         title: "Franz Schubert's Ständchen - Voice (Clarinet) & Piano",
-//         url:
-//             "https://www.mfiles.co.uk/mp3-downloads/franz-schubert-standchen-serenade.mp3"
-//     }
-// ];
 
 export default function MiniPlayer() {
   const [selectedTrack, setSelectedTrack] = useState(tracks[0]);
+  const [list, setList] = useState(tracks);
+
+  const handleList = () => {
+    setList(!list);
+  };
 
   return (
     <div className="mini-player">
-      <CoverMusic />
       <Wave url={selectedTrack.url} />
+      <button
+        className={list ? "playlist active" : "playlist"}
+        onClick={() => handleList}
+      >
+        music list
+      </button>
       <PlayList
         tracks={tracks}
         selectedTrack={selectedTrack}
         setSelectedTrack={setSelectedTrack}
+        testState="asdasd"
       />
       <br />
     </div>

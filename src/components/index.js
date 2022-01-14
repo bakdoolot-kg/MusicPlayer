0baa0b45a551664a6512d38c8d349994368b900b
bakdoolot-kg <altybaevbaku15@gmail.com>
2022-01-15 00:58:46 +0600

    Destruction files import

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
-export { default as Header } from "./Header/Header";
+// export { default as Header } from "./Header/Header";
 export { default as NavList } from "./NavList/NavList";
+export { default as MiniPlayer } from "./MiniPlayer/MiniPlayer";
+export { default as Layout } from "./Layout/Layout";
+export { default as Wave } from "./Wave/Wave";
+export { default as PlayList } from "./Playlist/PlayList";
+export { default as CoverMusic } from "./CoverMusic/CoverMusic";

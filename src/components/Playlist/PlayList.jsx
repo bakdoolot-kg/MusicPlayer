da87eb53cf8dee255e4688f3d16f6cf4de6c6230
bakdoolot-kg <altybaevbaku15@gmail.com>
2022-01-20 11:24:54 +0600

    Update Playlist.jsx

--- src/components/Playlist/PlayList.jsx
+++ src/components/Playlist/PlayList.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { tracks } from "../../bd";
+import { musicReducer, initialState } from "../../redux/musicReducer";
 
-const PlayList = ({ tracks, selectedTrack, setSelectedTrack }) => {
+const PlayList = (props) => {
   return (
     <div className="playlist">
       {tracks.map((track) => (
         <div
           key={track.id}
           className={
-            track.id === selectedTrack.id
+            track.id === props.selectedTrack
               ? "playlist-item selected"
               : "playlist-item"
           }
-          onClick={() => setSelectedTrack(track)}
+          onClick={() => props.setSelectedTrack(track)}
         >
-          {track.artist} <span> - </span>
+          {track.artist}
+          <span> - </span>
           {track.title}
         </div>
       ))}
@@ -21,5 +24,28 @@ const PlayList = ({ tracks, selectedTrack, setSelectedTrack }) => {
     </div>
   );
 };
+
+//TODO: Запасной вариант навсякий случай
+// const PlayList = ({ tracks, selectedTrack, setSelectedTrack }) => {
+//   return (
+//     <div className="playlist">
+//       {tracks.map((track) => (
+//         <div
+//           key={track.id}
+//           className={
+//             track.id === selectedTrack.id
+//               ? "playlist-item selected"
+//               : "playlist-item"
+//           }
+//           onClick={() => setSelectedTrack(track)}
+//         >
+//           {track.artist} <span> - </span>
+//           {track.title}
+//         </div>
+//       ))}
+//       {/* {props.testState} */}
+//     </div>
+//   );
+// };
 
 export default PlayList;

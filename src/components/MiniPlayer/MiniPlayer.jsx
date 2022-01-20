a8803a6aaaaddec4bb027b74681311f541eda716
Bakdoolot <altybaevbaku15@gmail.com>
2022-01-20 11:55:39 +0600

    Clear states

--- src/components/MiniPlayer/MiniPlayer.jsx
+++ src/components/MiniPlayer/MiniPlayer.jsx
@@ -5,27 +5,21 @@ import { tracks } from "../../bd";
 
 export default function MiniPlayer() {
   const [selectedTrack, setSelectedTrack] = useState(tracks[0]);
-  const [list, setList] = useState(tracks);
-
-  const handleList = () => {
-    setList(!list);
-  };
 
   return (
     <div className="mini-player">
       <Wave url={selectedTrack.url} />
-      <button
-        className={list ? "playlist active" : "playlist"}
-        onClick={() => handleList}
-      >
-        music list
-      </button>
-      <PlayList
-        tracks={tracks}
-        selectedTrack={selectedTrack}
-        setSelectedTrack={setSelectedTrack}
-        testState="asdasd"
-      />
+
+      <div className="mini-player__playlist">
+        <button>music list</button>
+
+        <PlayList
+          tracks={tracks}
+          selectedTrack={selectedTrack}
+          setSelectedTrack={setSelectedTrack}
+        />
+      </div>
+
       <br />
     </div>
   );

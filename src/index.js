2b145c39b363c8ce38726d723074abe9bd7d53b6
bakdoolot-kg <altybaevbaku15@gmail.com>
2022-01-20 11:19:11 +0600

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import "./index.scss";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, compose } from "redux";
+import { rootReducer } from "./redux/rootReducer";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
